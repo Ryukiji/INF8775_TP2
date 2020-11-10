5d034484ab13e36f5af60b789dc01134e92f6125
MouradYounes <mourad.younes@polymtl.ca>
2020-11-10 16:01:06 -0500

    Done with dynamic programming in c++

--- Code/progdyn.cpp
+++ Code/progdyn.cpp
@@ -3,8 +3,30 @@
 #include <tuple>
 #include <vector>
 #include <map>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+list<tuple<int,int,int>> generatePoints(string fileName) {
+    string line;
+    ifstream myfile (fileName);
+    list<tuple<int, int, int>> boxes;
+    if (myfile.is_open()) {
+        while ( getline (myfile,line) ) {
+            vector<int> coords;
+            stringstream ss(line);
+            string item;
+            while (getline(ss, item, ' ' )) {
+                coords.push_back(stoi(item));
+            }
+            boxes.push_back({make_tuple(coords[0], coords[1], coords[2])});
+        }
+        myfile.close();
+    }
+
+    return boxes; 
+}
 
 int sortSurface(tuple<int,int,int> point1, tuple<int,int,int> point2) {
     return get<1>(point1) * get<2>(point1) > get<1>(point2) * get<2>(point2);
@@ -58,13 +80,13 @@ list<tuple<int,int,int>> dynamicProgramming(list<tuple<int,int,int>>* points) {
     for ( int i = 0; i < n; i++ ) 
         if ( max < height[i] ) 
             max = height[i];
-
+    cout << max << endl;
 
     return answer;
 }
 
 int main() {
-    list<tuple<int,int,int>> points = {make_tuple(4, 6, 7), make_tuple(6, 4, 7), make_tuple(7, 4, 6), make_tuple(1, 2, 3), make_tuple(2, 1 ,3), make_tuple(3, 1 ,2), make_tuple(4, 5, 6), make_tuple(5, 4, 6), make_tuple(6, 4 ,5), make_tuple(10, 12, 32), make_tuple(12, 10, 32), make_tuple(32, 10, 12)};
+    list<tuple<int,int,int>> points = generatePoints("./b100_1.txt");
     list<tuple<int,int,int>> tower = dynamicProgramming(&points);
     for (tuple<int,int,int> box : tower) {
         cout << get<0>(box) << " " << get<1>(box) << " " << get<2>(box) << endl;
